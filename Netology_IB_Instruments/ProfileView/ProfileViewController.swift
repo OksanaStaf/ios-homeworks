9e76d352a381bb28b52f8382428655cc921559ed
Oksana Stafeeva <oksana.stafeeva89@yandex.ru>
2022-03-15 22:53:44 +0300

    Added Netology_IB_Instruments

---
+++ Netology_IB_Instruments/ProfileView/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  ProfileView
+//
+//  Created by OksanaS on 15.03.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ Netology_IB_Instruments/View.swift
@@ -0,0 +1,8 @@
+//
+//  View.swift
+//  ProfileView
+//
+//  Created by OksanaS on 15.03.2022.
+//
+
+import Foundation
