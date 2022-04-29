37e30589db043ae5d6c918c2565742e6931f9372
Oksana Stafeeva <oksana.stafeeva89@yandex.ru>
2022-04-30 00:15:52 +0300

    Corrected diploim

--- Navigation/TabBarController.swift
+++ Navigation/TabBarController.swift
@@ -12,7 +12,6 @@ class TabBarController: UITabBarController {
     private enum TabBarItem {
         case newstipe
         case profile
-        case gesture
         
         var title: String {
             switch self {
@@ -20,8 +19,6 @@ class TabBarController: UITabBarController {
                 return "Лента"
             case .profile:
                 return "Профиль"
-            case.gesture:
-                return "Анимация"
             }
         }
         
@@ -31,8 +28,6 @@ class TabBarController: UITabBarController {
                 return UIImage(systemName: "doc.text.fill")
             case .profile:
                 return UIImage(systemName: "person.crop.artframe")
-            case .gesture:
-                return UIImage(systemName: "dot.circle.and.hand.point.up.left.fill")
             }
         }
     }
@@ -43,15 +38,13 @@ class TabBarController: UITabBarController {
     }
     
     func setupTabBar() {
-        let items: [TabBarItem] = [ .newstipe, .profile, .gesture]
+        let items: [TabBarItem] = [ .newstipe, .profile]
         self.viewControllers = items.map({tabBarItem in
             switch tabBarItem {
             case .newstipe:
                 return UINavigationController(rootViewController: FeedViewController())
             case .profile:
-                return UINavigationController(rootViewController: LogInViewController()) //поменяли на др страничку вместо Profile
-            case .gesture:
-                return UINavigationController(rootViewController: AnimatedViewController())
+                return UINavigationController(rootViewController: LogInViewController())
             }
         })
         
